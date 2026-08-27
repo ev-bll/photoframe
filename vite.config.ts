a42dbfeba3ed9893a25a123f67b2980546dd75b4
Eva BL <evabll96@gmail.com>
2026-08-27 12:14:30 +0200

    install pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Photo Frame App',
+        short_name: 'PhotoFrame',
+        description: 'Add a white border to any photo',
+        theme_color: '#ffffff',
+        background_color: '#B9D8DA',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
